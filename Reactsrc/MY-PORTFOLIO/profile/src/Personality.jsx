d543247f257d016cffa7560792d0fd5357edf7e0
akenz1901 <michael_aka1@icloud.com>
2021-09-01 21:49:57 +0100

    Paragraph added and blended

--- Reactsrc/MY-PORTFOLIO/profile/src/Personality.jsx
+++ Reactsrc/MY-PORTFOLIO/profile/src/Personality.jsx
@@ -5,9 +5,12 @@ import './Personality.css'
 const Personality = () => {
     return(
     <div className="main">
-        <h3>
+        <h3 id ="top-about">
             I'm {user.name} a/an Entrepreneur and Software Engineer.
         </h3>
+        <p id="write-up">I specialize in rapidly prototyping software companies and web
+        applications. i talk about my journey on <a href="">Twitter</a>, comit code on <a href="">Github</a>, 
+        and take shots on <a href="">Dribble</a>.</p>
     </div>
     );
 }
