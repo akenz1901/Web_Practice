9544ae765ae5dcc01809f548b5afe164004f7f46
akenz1901 <michael_aka1@icloud.com>
2021-09-01 21:48:35 +0100

    Added new paragraph

--- Reactsrc/MY-PORTFOLIO/profile/src/reusableComponent/Header.jsx
+++ Reactsrc/MY-PORTFOLIO/profile/src/reusableComponent/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Love from '../logos/suit-heart.svg'
 import './Header.css'
+import Button from './Button'
 
 const user = {
     name: 'Akenz Mike'
@@ -10,12 +11,12 @@ const Header = () => {
     return(
         <div className = "top-header">
             <img id='icon' src= {Love}></img>
-            <h3 id='user'>{user.name}</h3>
+            <h2 id='user'>{user.name}</h2>
             <div className="link">
-                <a id='at'>Article</a>
-                <a id='sn'>Snippets</a>
-                <a id='ab'>About</a>
-                <a id='co'> <button className = "btn">Contact</button></a>
+                <a id='at' href="">Article</a>
+                <a id='sn' href="">Snippets</a>
+                <a id='ab' href="">About</a>
+                <a id='co' href=""> <Button Button = "Contact" height="0.9cm" color="white" backgroundColor="#282c34" borderRadius="0.1cm" borderStyle="solid"/></a>
             </div>
         </div>
     );
