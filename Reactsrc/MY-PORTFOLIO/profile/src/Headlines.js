ebfb5af56852d6a811d57b232fc4716f5616913b
akenz1901 <michael_aka1@icloud.com>
2021-10-01 21:56:00 +0100

    New Features had been added

---
+++ Reactsrc/MY-PORTFOLIO/profile/src/Headlines.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Headlines = () =>{
+    return(
+        <div>
+            <div>
+                <h4>Latest Articles</h4>
+                <a>All articles</a>
+            </div>
+            <div>
+                <img></img>
+                <p></p> 
+                <img></img>
+                <p></p> 
+                <img></img>
+                <p></p>
+            </div>
+        </div>
+    );
+}
+export default Headlines;
